d87825120fd10d1fe338449b0fdeff5a7200a6b9
HarveyHunt <harveyhuntnexus@gmail.com>
2013-12-16 22:05:37 +0000

    Removed redrawing logic from the main.py file and instead allowed the UI class to manage it. All elements that need redrawing are redrawn in one go as a batch, rather than each time an element changes (which can lead to as many as 4 redraws a second).

--- main.py
+++ main.py
@@ -27,26 +27,25 @@ class Stanza():
         while True:
             if not self.player.is_running():
                 exit()
-            self.player.check_status()
+            self.player.update_status()
             if self.status != self.player.status:
                 self.ui.set_footer_data(self.player.status)
                 self.ui.set_header_data(self.player.status)
-                self.ui.loop.draw_screen()
                 if self.status['artist'] != self.player.status['artist'] or \
                         self.status['album'] != self.player.status['album'] or \
                         self.status['title'] != self.player.status['title']:
                             self.status['artist'] = self.player.status['artist']
                             self.status['album'] = self.player.status['album']
                             self.status['title'] = self.player.status['title']
-                            md_thread = Thread(target=self.md.get('lyrics',
-                                self.status['artist'], self.status['album'],
-                                self.status['title']))
-                            md_thread.daemon = True
-                            md_thread.start()
+                            Thread(target=self.md.get, args=('lyrics', 
+                                self.status['artist'],
+                                self.status['album'],
+                                self.status['title'])).start()
                 self.status = self.player.status
             if self.lyrics != self.md.lyrics:
                 self.lyrics = self.md.lyrics
-                self.ui.set_listbox_data(self.lyrics.decode())
+                self.ui.set_listbox_data(self.lyrics)
+            if self.ui.is_dirty:
                 self.ui.loop.draw_screen()
             time.sleep(0.5)
 
